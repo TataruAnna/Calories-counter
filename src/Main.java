b6cbf6fa0a02f2e5487195598f4c9c5da58aa97b
TataruAnna <dublushq@gmail.com>
2023-08-06 18:37:35 +0300

    Add commit with all functions working

--- src/Main.java
+++ src/Main.java
@@ -9,27 +9,93 @@ public class Main {
             printMenu();
             option = scanner.nextInt();
             performSelectedAction(option, productCatalog, scanner);
+        } while (option != 6);
 
-        }while (option!=6);
+
+//        Product produs1 = new Product("mere", 20, 30, 40);
+//        System.out.println(productCatalog.addProduct(produs1));
+//        System.out.println("Ai introdus in total: " + productCatalog.numberOfProducts + " produse");
+//
+//        Product produs2 = new Product("mere", 20, 30, 40);
+//        System.out.println(productCatalog.addProduct(produs2));
+//        System.out.println("Ai introdus in total: " + productCatalog.numberOfProducts + " produse");
+//
+//
+//        Product produs3 = new Product("pere", 10, 40, 50);
+//        System.out.println(productCatalog.addProduct(produs3));
+//        System.out.println("Ai introdus in total: " + productCatalog.numberOfProducts + " produse");
+//
+//        System.out.println("Introdu numele produsului pe care vrei sa il stergi");
+//        String nameToBeDeleted = scanner.next();
+//        System.out.println(productCatalog.deleteProduct(nameToBeDeleted));
+
+//        System.out.println(productCatalog.deleteProduct(produs3));
+//        productCatalog.printProducts();
+//
+//        System.out.println("Produsul a fost gasit: " +productCatalog.getProductByName("mere"));
+
+
+//        System.out.println(produs1.toString());
+//        System.out.println(produs2.toString());
+//        System.out.println(produs3.toString());
+
+//        productCatalog.addProduct(produs2);
+//        productCatalog.addProduct(produs3);
+//
+//
+//        productCatalog.printProducts();
+//        calculeaza caloriile unui produs fara a fii adaugat in catalog
+//        Product tempProduct = Product.getInfo();
+//        System.out.println("Caloriile produsului sunt: " + Product.computeCalories(tempProduct.fats, tempProduct.carbs, tempProduct.proteins));
     }
 
-    public static void printMenu(){
+    public static void printMenu() {
+        System.out.println("---------------");
+        System.out.println("meniu");
+        System.out.println("1. Adauga un produs in calculator si calculeaza-i caloriile");
+        System.out.println("2. Calculeaza caloriile pentru un produs fara a fi adaugat in catalog");
+        System.out.println("3. Afiseaza toate produsele din catalog si caloriile pentru fiecare");
+        System.out.println("4. Sterge un produs din catalog");
+        System.out.println("5. Gaseste produs dupa nume");
+        System.out.println("6. Iesi din meniu");
+        System.out.println("---------------");
+        System.out.println("introdu optiunea aleasa");
 
     }
 
-    public static void performSelectedAction(int option, ProductCatalog productCatalog, Scanner scanner){
-        switch (option){
+    public static void performSelectedAction(int option, ProductCatalog productCatalog, Scanner scanner) {
+        switch (option) {
             case 1:
-                //citim de la tastatura detaliile produsului
-                //ne instantiem un produs cu aceste detalii
-                //pasam obiectul creat la metoda addProduct
-                String name = scanner.nextLine();
-                double fats = scanner.nextDouble();
-                double carbs = scanner.nextDouble();
-                double proteins = scanner.nextDouble();
-                Product newProduct = new Product(name, fats, carbs, proteins);
-                boolean result = productCatalog.addProduct(newProduct);
+                Product product = Product.getInfo();
+                System.out.println(productCatalog.addProduct(product));
+                System.out.println("Ai introdus: " + productCatalog.numberOfProducts + " produse");
+                break;
 
+            case 2:
+                Product tempProduct = Product.getInfo();
+                System.out.println("Caloriile produsului sunt: " + Product.computeCalories(tempProduct.fats, tempProduct.carbs, tempProduct.proteins));
+                break;
+            case 3:
+                System.out.println("Produsele catalogului sunt:");
+                productCatalog.printProducts();
+                System.out.println();
+                break;
+            case 4:
+                System.out.println("Introdu numele produsului pe care vrei sa il stergi");
+                String nameToBeDeleted = scanner.next();
+                System.out.println(productCatalog.deleteProduct(nameToBeDeleted));
+                break;
+
+            case 5:
+                System.out.println("Specifica numele produsului pe care vrei sa il gasesti ");
+                String findByName = scanner.next();
+                System.out.println(productCatalog.getProductByName(findByName));
+
+                break;
+            case 6:
+                break;
+            default:
+                System.out.println("optiune invalida. Reintrodu numarul");
         }
-}
+    }
 }
--- src/Product.java
+++ src/Product.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Product {
 
     String name;
@@ -13,9 +15,33 @@ public class Product {
         this.proteins = proteins;
         this.calories = computeCalories(fats, carbs, proteins);
     }
+    public static Product getInfo(){ //functie independenta de obiect dar a clasei care citeste de la tastatura
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introdu numele produsului:");
+        String name = scanner.next();
+        System.out.println("Introdu grasimile:");
+        double fats = scanner.nextDouble();
+        System.out.println("introdu carbohidratii:");
+        double carbs = scanner.nextDouble();
+        System.out.println("introdu proteinele:");
+        double proteins = scanner.nextDouble();
 
-    public static double computeCalories(double fats, double carbs, double proteins){
+        return new Product(name,fats,carbs,proteins);
+    }
+    public static double computeCalories(double fats, double carbs, double proteins) {
         double calories = 0;
+        calories = (9 * fats + 4 * carbs + 4 * proteins);
         return calories;
+    }
+
+    @Override
+    public String toString() {
+        return "Produsul cu numele " + '\'' +
+                  name + '\'' +
+                ", care are: " + fats +
+                " grasimi, " + carbs +
+                " carbohidrati, " + proteins +
+                " proteine  si " + calories +
+                " calorii";
     }
 }
